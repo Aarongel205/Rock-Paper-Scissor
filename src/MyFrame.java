5b40fe46f49806e36ba8e4b20a3d946556f67149
Aarongel205 <aaronadanza92@gmail.com>
2025-05-09 00:54:36 +0800

    change background to black

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -23,39 +23,53 @@ public class MyFrame extends JFrame implements ActionListener{
         choices.add("✌");
         choices.add("✋");
 
+        JPanel computerPanel = new JPanel();
+        computerPanel.setPreferredSize(new Dimension(100, 500));
+
+        JPanel userPanel = new JPanel();
+        userPanel.setPreferredSize(new Dimension(100, 500));
+
         JPanel resultPanel = new JPanel();
-        resultPanel.setBackground(Color.ORANGE);
+        resultPanel.setBackground(Color.BLACK);
+        resultPanel.setPreferredSize(new Dimension(200, 300));
 
         JPanel scorePanel = new JPanel();
         scorePanel.setLayout(new GridLayout(1, 2));
 
         computerResult = new JLabel("Computer score: " + Integer.toString(computerScore));
+        computerResult.setForeground(Color.WHITE);
         computerResult.setHorizontalAlignment(JLabel.CENTER);
-        computerResult.setBackground(Color.RED);
+        computerResult.setBackground(Color.BLACK);
         computerResult.setOpaque(true);
 
         playerResult = new JLabel("Your score: " + Integer.toString(playerScore));
-        playerResult.setVerticalAlignment(JLabel.CENTER);
-        playerResult.setBackground(Color.BLUE);
+        playerResult.setForeground(Color.WHITE);
+        playerResult.setHorizontalAlignment(JLabel.CENTER);
+        playerResult.setBackground(Color.BLACK);
         playerResult.setOpaque(true);
 
         matchResult = new JLabel("V.S");
+        matchResult.setForeground(Color.WHITE);
+        matchResult.setHorizontalAlignment(JLabel.CENTER);
         matchResult.setBackground(Color.YELLOW);
-        matchResult.setFont(matchResult.getFont().deriveFont(30f));
+        matchResult.setFont(matchResult.getFont().deriveFont(20f));
+        matchResult.setBackground(Color.BLACK);
         matchResult.setOpaque(true);
 
         user = new JLabel();
-        user.setBackground(Color.BLACK);
+        user.setForeground(Color.WHITE);
         user.setFont(user.getFont().deriveFont(90f));
         user.setHorizontalAlignment(JLabel.CENTER);
         user.setVerticalAlignment(JLabel.CENTER);
+        user.setBackground(Color.BLACK);
         user.setOpaque(true);
 
         computer = new JLabel();
-        computer.setBackground(Color.BLACK);
+        computer.setForeground(Color.WHITE);
         computer.setFont(user.getFont().deriveFont(90f));
         computer.setHorizontalAlignment(JLabel.CENTER);
         computer.setVerticalAlignment(JLabel.CENTER);
+        computer.setBackground(Color.BLACK);
         computer.setOpaque(true);
 
 
@@ -86,12 +100,17 @@ public class MyFrame extends JFrame implements ActionListener{
         this.add(user, BorderLayout.WEST);
         this.add(computer, BorderLayout.EAST);
 
+        userPanel.add(user);
+        computerPanel.add(computer);
         resultPanel.add(matchResult);
         scorePanel.add(playerResult);
         scorePanel.add(computerResult);
         this.add(resultPanel, BorderLayout.CENTER);
         this.add(scorePanel, BorderLayout.NORTH);
+        this.add(userPanel, BorderLayout.WEST);
+        this.add(computerPanel, BorderLayout.EAST);
 
+        this.getContentPane().setBackground(Color.BLACK);
         this.setVisible(true);
     }
     public String result(){
